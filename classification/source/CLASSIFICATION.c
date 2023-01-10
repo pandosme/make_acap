6f7a57fa0cb745418ddc2ddb67b7bfec5e1f884d
Fred Juhlin <fredj@axis.com>
2023-01-10 21:24:04 +0000

    Cleanup

--- classification/source/CLASSIFICATION.c
+++ classification/source/CLASSIFICATION.c
@@ -21,8 +21,8 @@
 
 #define LOG(fmt, args...)    	{ syslog(LOG_INFO, fmt, ## args);  printf(fmt, ## args); }
 #define LOG_WARN(fmt, args...)  { syslog(LOG_WARNING, fmt, ## args); printf(fmt, ## args);}
-#define LOG_TRACE(fmt, args...)    { syslog(LOG_INFO, fmt, ## args); printf(fmt, ## args);}
-//#define LOG_TRACE(fmt, args...) {}
+//#define LOG_TRACE(fmt, args...)    { syslog(LOG_INFO, fmt, ## args); printf(fmt, ## args);}
+#define LOG_TRACE(fmt, args...) {}
 
 /*
 typedef struct {
